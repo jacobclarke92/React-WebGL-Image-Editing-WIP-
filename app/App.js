f43153681137847703f796ea0b0d8851fff4a349
Jacob Clarke <jacobclarke92@gmail.com>
2016-03-16 23:17:56 +1000

    Added a third test image

--- app/App.js
+++ app/App.js
@@ -7,6 +7,7 @@ export default class App extends Component {
 
 	constructor(props) {
 		super(props);
+		this.urls = ['test1.jpg', 'test2.jpg', 'test3.jpg'];
 		this.state = {
 			url: 'test1.jpg',
 			width: 550,
@@ -27,11 +28,12 @@ export default class App extends Component {
 		const { url, width, height } = this.state;
 		return (
 			<div>
+				<p>
+					{this.urls.map((url, i) => 
+						<button key={i} onClick={() => this.setUrl(url)}>Image {i+1}</button>
+					)}
+				</p>
 				<Editor url={url} width={width} height={height} onResize={this.handleImageResize} />
-				<div>
-					<button onClick={() => this.setUrl('test1.jpg')}>Image 1</button>
-					<button onClick={() => this.setUrl('test2.jpg')}>Image 2</button>
-				</div>
 			</div>
 		)
 	}
