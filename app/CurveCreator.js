8cc279de57929067d920736658d7c754bfbafad4
Jacob Clarke <jacobclarke92@gmail.com>
2016-05-02 15:57:05 +1000

    Added working mouse events to CurvesCreator still WIP

--- app/CurveCreator.js
+++ app/CurveCreator.js
@@ -2,11 +2,20 @@ import React, { Component, PropTypes } from 'react'
 
 import { curvesHashTable } from 'editor/utils/colorUtils'
 
+function getElementMousePosition(event, element) {
+	const rect = element.getBoundingClientRect();
+	if(!event) return {x: 0, y: 0};
+	if(!element) return {x: event.clientX, y: event.clientY};
+	return {x: event.clientX - rect.left, y: event.clientY - rect.top};
+}
+
 export default class CurveCreator extends Component {
 	
 	constructor(props) {
 		super(props);
+		this.capturingMouseMove = false;
 		this.handleMouseMove = this.handleMouseMove.bind(this);
+		this.handleMouseUp = this.handleMouseUp.bind(this);
 		this.state = {
 			points: [],
 		};
@@ -14,14 +23,31 @@ export default class CurveCreator extends Component {
 
 	componentDidMount() {
 		document.addEventListener('mousemove', this.handleMouseMove);
+		document.addEventListener('mouseup', this.handleMouseUp);
 	}
 
 	componentWillUnmount() {
 		document.removeEventListener('mousemove', this.handleMouseMove);
+		document.removeEventListener('mouseup', this.handleMouseUp);
 	}
 
-	handleMouseDown(event) {
+	handleMouseMove(event) {
+		if(this.capturingMouseMove) {
+			const mousePosition = getElementMousePosition(event, this.refs.curveCreator);
+			console.log(mousePosition);
+		}
+	}
 
+	handleMouseUp(event) {
+		this.capturingMouseMove = false;
+	}
+
+	handleMouseDown(event, createNew = false) {
+		this.capturingMouseMove = true;
+		if(createNew) {
+			const mousePosition = getElementMousePosition(event, this.refs.curveCreator);
+			console.log('Create new point', mousePosition);
+		}
 	}
 
 	render() {
@@ -29,10 +55,10 @@ export default class CurveCreator extends Component {
 		const { points } = this.state;
 
 		return (
-			<div className="curve-creator grid-pattern" onMouseDown={::this.handleMouseDown}>
+			<div ref="curveCreator" className="curve-creator grid-pattern" onMouseDown={event => this.handleMouseDown(event, true)}>
 				<canvas ref="curveLine" className="curve-line" />
 				{points.map((point, i) => 
-					<div className="curve-point" styles={{left: point.x, top: point.y}} />
+					<div className="curve-point" styles={{left: point.x, top: point.y}} onMouseDown={::this.handleMouseMove} />
 				)}
 			</div>
 		)
