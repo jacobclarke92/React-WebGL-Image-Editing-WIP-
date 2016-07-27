42f5a8824747da7f8f007a23a52c730f7c50bd26
Jacob Clarke <jacob@bigfish.tv>
2016-07-27 17:30:28 +1000

    started a node server

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var express = require('express')
+var app = express()
 
-new WebpackDevServer(webpack(config), {
-	publicPath: config.output.publicPath,
-	hot: true,
-	historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-	if (err) {
-		return console.log(err);
-	}
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname))
 
-	console.log('Listening at http://localhost:3000/');
-});
+app.get('/', function(request, response) {
+	response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+	console.log("Node app is running at localhost:" + app.get('port'))
+})
