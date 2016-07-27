4c7387ccd65604e4a97579840ded6c5739d489ac
Jacob Clarke <jacob@bigfish.tv>
2016-07-27 19:01:37 +1000

    aaaand again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', 5000);
+app.set('port', 8080);
 app.use(express.static(__dirname));
 
 app.get('/', function(request, response) {
