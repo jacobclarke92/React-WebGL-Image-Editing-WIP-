c3dd97f954e1cd47d03bfb25906179aa38c9d7f7
Lightspeed <nicholas.paustian@gmail.com>
2016-12-01 23:50:24 +1100

    Moved the settings into a tray to the right.

--- app/Editor.js
+++ app/Editor.js
@@ -161,17 +161,17 @@ const curveAdjustmentProperties = [
 	},
 	{
 		label: 'curves_red',
-		channels: 'r', 
+		channels: 'r',
 		defaultValue: {channels: 'r', curves: [[0,0], [255,255]]},
 	},
 	{
 		label: 'curves_green',
-		channels: 'g', 
+		channels: 'g',
 		defaultValue: {channels: 'g', curves: [[0,0], [255,255]]},
 	},
 	{
 		label: 'curves_blue',
-		channels: 'b', 
+		channels: 'b',
 		defaultValue: {channels: 'b', curves: [[0,0], [255,255]]},
 	},
 ];
@@ -206,7 +206,7 @@ export default class Editor extends Component {
 
 		// store reset point, make a copy of adjustments object
 		this.defaultAdjustments = {...adjustments};
-		
+
 		this.state = {
 			url: this.urls[Math.floor(Math.random()*this.urls.length)],
 			width: 550,
@@ -254,7 +254,7 @@ export default class Editor extends Component {
 		// generate editSteps for Renderer
 		const adjustmentSteps = this.generateEditStepsFromAdjustments(adjustments);
 		const editSteps = [...adjustmentSteps, ...filterSteps];
-		
+
 		this.setState({adjustments, adjustmentSteps, editSteps});
 	}
 
@@ -278,8 +278,8 @@ export default class Editor extends Component {
 		Object.keys(adjustments).map(adjustment => {
 			const adjustmentValue = adjustments[adjustment];
 			const adjustmentProperties = [
-				...tonalAdjustmentProperties, 
-				...enhancementAdjustmentProperties, 
+				...tonalAdjustmentProperties,
+				...enhancementAdjustmentProperties,
 				...curveAdjustmentProperties,
 				colorMapAdjustment,
 			];
@@ -336,112 +336,117 @@ export default class Editor extends Component {
 
 		return (
 			<div className="image-editor">
-				<h1>React WebGL Image Editing</h1>
-				<div className="images">
-					<label>Select an image below or drag your own onto the window</label>
-					{this.urls.map((_url, i) => 
-						<button key={i} onClick={() => this.setUrl(_url)} disabled={url == _url}>Image {i+1}</button>
-					)}
+				<div className="main-window">
+					<FileDropzone onFilesReceived={::this.handleReceivedFile}>
+						<div className="canvas-wrapper" style={{backgroundImage:'url('+url+')', maxWidth: width}}>
+							<Renderer url={url} width={width} height={height} onResize={::this.handleImageResize} editSteps={editSteps} autoResize />
+						</div>
+					</FileDropzone>
 				</div>
-				<div className="filters">
-					<label>Select a preset (optional)</label>
-					<button onClick={() => this.setFilter({title: null, steps: []})} disabled={!filterName}>
-						<img src={url} width={thumbnailWidth} height={thumbnailHeight} onMouseDown={e => e.preventDefault()} />
-						<br />
-						Original
-					</button>
-					{filterPresets.map((filterPreset, i) => (
-						<button key={i} onClick={() => this.setFilter(filterPreset)} disabled={filterPreset.name === filterName}>
-							<Renderer url={url} width={thumbnailWidth} height={thumbnailHeight} editSteps={this.generateEditStepsFromFilterPreset(filterPreset)} onRender={() => console.log(filterPreset.title, 'rendered!')} />
-							<br />
-							{filterPreset.title}
-						</button>
-					))}
-				</div>
-				<label>
-					Upload Preset <br />
-					<input className="button" type="file" onChange={event => this.handleReceivedFile(event.nativeEvent.target.files)} />
-				</label>
-				<Tabs>
-					<Panel title="Tonal Adjustments">
-						<div className="sliders">
-							{Object.keys(adjustments).filter(key => tonalPropertyLabels.indexOf(key) >= 0).map((key, i) => {
-								const { label, ...inputAttrs } = tonalAdjustmentProperties.filter(effect => effect.label === key)[0];
-								return (
-									<label key={'tonal'+i}>
-										<div>{titleize(label)}</div>
-										<RCSlider {...inputAttrs} value={adjustments[key]} onChange={value => this.setValue(key, value)} included={inputAttrs.min < 0} marks={inputAttrs.defaultValue > inputAttrs.min ? {[inputAttrs.defaultValue]: inputAttrs.defaultValue} : {}} />
-									</label>
-								)
-							})}
+				<div className="tray">
+					<div className="tray-inner">
+						<div className="images">
+							<label>Select an image below or drag your own onto the window</label>
+							{this.urls.map((_url, i) =>
+								<button key={i} onClick={() => this.setUrl(_url)} disabled={url == _url}>Image {i+1}</button>
+							)}
 						</div>
-					</Panel>
-					<Panel title="Curves">
-						{Object.keys(adjustments).filter(key => curvePropertyLabels.indexOf(key) >= 0).map((key, i) => {
-							const { label, channels } = curveAdjustmentProperties.filter(effect => effect.label === key)[0];
-							const points = adjustments[key].curves;
-							return (
-								<label key={'curve'+i} className="curve-label">
-									<div>{titleize(label.replace('_', ' '))}</div>
-									<CurveCreator defaultValue={points} size={200} onChange={points => this.setValue(key, {channels, curves: points})} />
-								</label>
-							)
-						})}
-					</Panel>
-					<Panel title="Enhancements">
-						<div className="sliders">
-							{Object.keys(adjustments).filter(key => enhancementPropertyLabels.indexOf(key) >= 0).map((key, i) => {
-								const { label, ...inputAttrs } = enhancementAdjustmentProperties.filter(effect => effect.label === key)[0];
-								return (
-									<label key={'enhancement'+i}>
-		                                <div>{titleize(label)}</div>
-										<RCSlider {...inputAttrs} value={adjustments[key]} onChange={value => this.setValue(key, value)} included={inputAttrs.min < 0} marks={inputAttrs.defaultValue > inputAttrs.min ? {[inputAttrs.defaultValue]: inputAttrs.defaultValue} : {}} />
-									</label>
-								)
-							})}
+						<div className="filters">
+							<label>Select a preset (optional)</label>
+							<button onClick={() => this.setFilter({title: null, steps: []})} disabled={!filterName}>
+								<img src={url} width={thumbnailWidth} height={thumbnailHeight} onMouseDown={e => e.preventDefault()} />
+								<br />
+								Original
+							</button>
+							{filterPresets.map((filterPreset, i) => (
+								<button key={i} onClick={() => this.setFilter(filterPreset)} disabled={filterPreset.name === filterName}>
+									<Renderer url={url} width={thumbnailWidth} height={thumbnailHeight} editSteps={this.generateEditStepsFromFilterPreset(filterPreset)} onRender={() => console.log(filterPreset.title, 'rendered!')} />
+									<br />
+									{filterPreset.title}
+								</button>
+							))}
 						</div>
-					</Panel>
-					<Panel title="Color Map">
-						<div className="input">
-							<label>Gradient Presets</label>
-							<Select 
-								items={gradientPresets}
-								onChange={value => this.setValue('colorMap', value.markers)}
-								Item={props => 
-									<div>
-										<label>{props.item.title}</label>
-										<Gradient markers={props.item.markers} width={200} height={30} />
-									</div>
-								} />
-						</div>
-						<GradientCreator value={adjustments['colorMap']} onChange={value => this.setValue('colorMap', value)} />
-					</Panel>
-					{/*
-					<Panel title="HSL Adjustments">
+						<label>
+							Upload Preset <br />
+							<input className="button" type="file" onChange={event => this.handleReceivedFile(event.nativeEvent.target.files)} />
+						</label>
 						<Tabs>
-							<Panel title="Hue">
-								<RCSlider value={this.state.hueAmount} min={-1} max={1} step={0.01} onChange={value => {this.setValue('hueAdjustment', {color: [66, 255, 7], value, range: this.state.hueRange}); this.setState({hueAmount: value})}} />
-								<RCSlider value={this.state.hueRange} min={0} max={1} step={0.01} onChange={range => {this.setValue('hueAdjustment', {color: [66, 255, 7], value: this.state.hueAmount, range}); this.setState({hueRange: range})}} />
+							<Panel title="Tonal Adjustments">
+								<div className="sliders">
+									{Object.keys(adjustments).filter(key => tonalPropertyLabels.indexOf(key) >= 0).map((key, i) => {
+										const { label, ...inputAttrs } = tonalAdjustmentProperties.filter(effect => effect.label === key)[0];
+										return (
+											<label key={'tonal'+i}>
+												<div>{titleize(label)}</div>
+												<RCSlider {...inputAttrs} value={adjustments[key]} onChange={value => this.setValue(key, value)} included={inputAttrs.min < 0} marks={inputAttrs.defaultValue > inputAttrs.min ? {[inputAttrs.defaultValue]: inputAttrs.defaultValue} : {}} />
+											</label>
+										)
+									})}
+								</div>
 							</Panel>
-							<Panel title="Luminance">
-								<div>Hi</div>
+							<Panel title="Curves">
+								{Object.keys(adjustments).filter(key => curvePropertyLabels.indexOf(key) >= 0).map((key, i) => {
+									const { label, channels } = curveAdjustmentProperties.filter(effect => effect.label === key)[0];
+									const points = adjustments[key].curves;
+									return (
+										<label key={'curve'+i} className="curve-label">
+											<div>{titleize(label.replace('_', ' '))}</div>
+											<CurveCreator defaultValue={points} size={200} onChange={points => this.setValue(key, {channels, curves: points})} />
+										</label>
+									)
+								})}
 							</Panel>
-							<Panel title="Saturation">
-								<div>Hello</div>
+							<Panel title="Enhancements">
+								<div className="sliders">
+									{Object.keys(adjustments).filter(key => enhancementPropertyLabels.indexOf(key) >= 0).map((key, i) => {
+										const { label, ...inputAttrs } = enhancementAdjustmentProperties.filter(effect => effect.label === key)[0];
+										return (
+											<label key={'enhancement'+i}>
+				                                <div>{titleize(label)}</div>
+												<RCSlider {...inputAttrs} value={adjustments[key]} onChange={value => this.setValue(key, value)} included={inputAttrs.min < 0} marks={inputAttrs.defaultValue > inputAttrs.min ? {[inputAttrs.defaultValue]: inputAttrs.defaultValue} : {}} />
+											</label>
+										)
+									})}
+								</div>
 							</Panel>
+							<Panel title="Color Map">
+								<div className="input">
+									<label>Gradient Presets</label>
+									<Select
+										items={gradientPresets}
+										onChange={value => this.setValue('colorMap', value.markers)}
+										Item={props =>
+											<div>
+												<label>{props.item.title}</label>
+												<Gradient markers={props.item.markers} width={200} height={30} />
+											</div>
+										} />
+								</div>
+								<GradientCreator value={adjustments['colorMap']} onChange={value => this.setValue('colorMap', value)} />
+							</Panel>
+							{/*
+							<Panel title="HSL Adjustments">
+								<Tabs>
+									<Panel title="Hue">
+										<RCSlider value={this.state.hueAmount} min={-1} max={1} step={0.01} onChange={value => {this.setValue('hueAdjustment', {color: [66, 255, 7], value, range: this.state.hueRange}); this.setState({hueAmount: value})}} />
+										<RCSlider value={this.state.hueRange} min={0} max={1} step={0.01} onChange={range => {this.setValue('hueAdjustment', {color: [66, 255, 7], value: this.state.hueAmount, range}); this.setState({hueRange: range})}} />
+									</Panel>
+									<Panel title="Luminance">
+										<div>Hi</div>
+									</Panel>
+									<Panel title="Saturation">
+										<div>Hello</div>
+									</Panel>
+								</Tabs>
+							</Panel>
+							*/}
 						</Tabs>
-					</Panel>
-					*/}
-				</Tabs>
-				<div>
-					<button onClick={event => this.handleReset()}>Reset</button>
-				</div>
-				<FileDropzone onFilesReceived={::this.handleReceivedFile}>
-					<div className="canvas-wrapper" style={{backgroundImage:'url('+url+')', maxWidth: width}}>
-						<Renderer url={url} width={width} height={height} onResize={::this.handleImageResize} editSteps={editSteps} autoResize />
+						<div>
+							<button onClick={event => this.handleReset()}>Reset</button>
+						</div>
 					</div>
-				</FileDropzone>
-				<div className="text-center">
+				</div>
+				<div className="text-center some-bs">
 					<div className="input" style={{marginBottom: 30}}>
 						<label>Terminal command</label>
 						<Textarea value={'babel-node backend/index.js input='+(url.indexOf('data:') === 0 ? '[[filepath]]' : url)+' editSteps="'+JSON.stringify(editSteps).split('"').join('\\"')+'"'} readOnly onClick={event => {event.target.focus(); event.target.select()}} />
