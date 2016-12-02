5a64cd46bf8b418cbf3c890cdad16285d6de5d74
Jacob Clarke <jacob@bigfish.tv>
2016-12-02 16:34:39 +1000

    Allow values of zero to still be passed to uniforms

--- app/editor/shaders/default_update.js
+++ app/editor/shaders/default_update.js
@@ -1,6 +1,6 @@
 // called within context of a Program
 export default function(settings) {
-	if(settings.key && settings.value) {
+	if(settings.key && (settings.value || settings.value === 0)) {
 		// console.log('updating uniform:', settings.key, settings.value);
 		this.uniforms({
 			[settings.key]: settings.value,
