6ad3541ae60d32e3871e3f8f32c7b3b944aba782
Jacob Clarke <jacob@bigfish.tv>
2017-01-10 15:12:51 +1000

    Modified test.js to render an already modified image

--- app/test.js
+++ app/test.js
@@ -1,245 +1,83 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import WebGLDebug from 'webgl-debug'
+import _find from 'lodash/find'
 
+import Renderer from './Renderer'
 
-import Shaders from './editor/shaders'
-import Program from './editor/Program'
-import Texture from './editor/Texture'
-import Framebuffer from './editor/Framebuffer'
-
-const useDebugger = false;
-
-function throwOnGLError(err, funcName, args) {
-   throw WebGLDebug.glEnumToString(err) 
-   + 'was caused by call to ' 
-   + funcName;
-}
+const preset1 = [
+	{
+		"name": "utility",
+		"steps": [
+			{
+				"key": "rotate",
+				"value": 90
+			}
+		]
+	},
+	{
+		"name": "adjustments",
+		"steps": []
+	},
+	{
+		"name": "filter",
+		"amount": 1,
+		"steps": []
+	}
+];
 
 class App extends Component {
 
-	constructor() {
-		super();
+	static defaultProps = {
+		url: 'test3.jpg',
+		instructions: preset1,
+	};
+
+	constructor(props) {
+		super(props);
 		this.state = {
-			rotate: 0,
-			width: 640,
-			height: 640,
-			canvasWidth: 640,
-			canvasHeight: 480,
+			width: 550,
+			height: 400,
+			canvasWidth: 550,
+			canvasHeight: 400,
 		}
 	}
 
-	componentDidMount() {
-		this.currentFramebufferIndex = -1;
-		this.image = new Image();
-		this.image.onload = () => this.handleImageLoad(this.image);
-		this.canvas = this.refs.editor;
-		this.gl = this.canvas.getContext('experimental-webgl');
-		if(!this.gl) this.gl = this.canvas.getContext('webgl');
+	handleResize(width, height) {
+		console.log('NEW IMAGE DIMENSIONS', width, height);
 
-		// this.gl = WebGLDebug.makeDebugContext(this.gl, throwOnGLError)
+		const { instructions } = this.props;
+		let canvasWidth = width;
+		let canvasHeight = height;
 
-		this.defaultProgram = new Program('default', this.gl, Shaders.default.vertex, Shaders.default.fragment, Shaders.default.update);
-		this.rotateProgram = new Program('default', this.gl, Shaders.rotate.vertex, Shaders.rotate.fragment, Shaders.rotate.update);
-		this.saturationProgram = new Program('saturation', this.gl, Shaders.saturation.vertex, Shaders.saturation.fragment, Shaders.saturation.update);
-		this.gammaProgram = new Program('gamma', this.gl, Shaders.gamma.vertex, Shaders.gamma.fragment, Shaders.gamma.update);
-		this.blendProgram = new Program('blend', this.gl, Shaders.blend.vertex, Shaders.blend.fragment, Shaders.blend.update);
-		this.testProgram = new Program('default', this.gl, Shaders.rotate.vertex, Shaders.default.fragment, Shaders.default.update);
+		const utilitySteps = _find(instructions, {name: 'utility'}).steps || [];
+		const rotateStep = _find(utilitySteps, {key: 'rotate'});
+		const cropStep = _find(utilitySteps, {key: 'crop'});
 		
-		this.framebuffer1 = new Framebuffer(this.gl).use();
-		this.framebuffer1.attachEmptyTexture(this.props.width, this.props.height);
-		this.framebuffer2 = new Framebuffer(this.gl).use();
-		this.framebuffer2.attachEmptyTexture(this.props.width, this.props.height);
-		this.editGroupFramebuffer = new Framebuffer(this.gl).use();
-		this.editGroupFramebuffer.attachEmptyTexture(this.props.width, this.props.height);
+		if(rotateStep && (rotateStep.value == 90 || rotateStep.value == 270)) {
+			canvasWidth = height;
+			canvasHeight = width;
+		}
 
-		this.image.src = '/test2__.jpg';
-	}
+		if(cropStep && cropStep.value) {
+			canvasWidth = Math.floor(canvasWidth * cropStep.value.width);
+			canvasHeight = Math.floor(canvasHeight * cropStep.value.height);
+		}
 
-	handleImageLoad(image) {
+		console.log('ACTUAL IMAGE DIMENSIONS', canvasWidth, canvasHeight);
 
-		this.imageTexture = new Texture(this.gl, image.width, image.height);
-		this.imageTexture.loadContentsOf(image);
-
-		const width = image.width;
-		const height = image.height;
-
-		this.setState({width, height, canvasWidth: width, canvasHeight: height}, () => {
-			this.resizeAll();
-			this.renderEditSteps();
-		});
-	}
-
-	resizeAll() {
-		const { canvasWidth, canvasHeight } = this.state;
-		this.gl.viewport(0, 0, canvasWidth, canvasHeight);
-		this.defaultProgram.resize(canvasWidth, canvasHeight);
-		this.rotateProgram.resize(canvasWidth, canvasHeight);
-		this.saturationProgram.resize(canvasWidth, canvasHeight);
-		this.gammaProgram.resize(canvasWidth, canvasHeight);
-		this.blendProgram.resize(canvasWidth, canvasHeight);
-		this.testProgram.resize(canvasWidth, canvasHeight);
-		this.framebuffer1.resizeTexture(canvasWidth, canvasHeight);
-		this.framebuffer2.resizeTexture(canvasWidth, canvasHeight);
-		this.editGroupFramebuffer.resizeTexture(canvasWidth, canvasHeight);
-	}
-
-	renderEditSteps() {
-		const { rotate } = this.state;
-
-		// default program step
-		let program = this.defaultProgram;
-		let sourceTexture = this.imageTexture;
-		let target = this.framebuffer1;
-
-		program.use();
-		program.update({key: 'default'});
-
-		program.willRender();
-		sourceTexture.use();
-		
-		target.use();//this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, target);
-
-		program.didRender();
-		program.draw();
-
-		if(useDebugger) debugger;
-
-
-		//rotate step
-		program = this.rotateProgram;
-		sourceTexture = this.framebuffer1.texture;
-		target = this.framebuffer2;
-
-		program.use();
-		program.update({key: 'rotate', value: rotate});
-		program.willRender();
-		sourceTexture.use();
-
-		target.use();//this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, target);
-
-		program.didRender();
-		program.draw();
-
-		if(useDebugger) debugger;
-
-		//saturation step
-		program = this.saturationProgram;
-		sourceTexture = this.framebuffer2.texture;
-		target = this.framebuffer1;
-
-		program.use();
-		program.update({key: 'saturation', value: 0.6});
-		program.willRender();
-		sourceTexture.use();
-
-		target.use();//this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, target);
-
-		program.didRender();
-		program.draw();
-
-		if(useDebugger) debugger;
-
-
-		// copy texture reference to group framebuffer
-		// const editGroupTextureId = 7;
-		// this.editGroupFramebuffer.use();
-		
-		target = this.editGroupFramebuffer;
-		// sourceTexture.use();
-		target.use();
-		// program.didRender();
-		program.draw();
-
-		if(useDebugger) debugger;
-
-
-
-		// test drawing group framebuffer to cavnas
-		// program = this.testProgram;
-		// sourceTexture = this.editGroupFramebuffer.texture;
-		// target = null;
-
-		// program.use();
-		// program.update({key: 'default'});
-		// program.willRender();
-		// sourceTexture.use();
-		// this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, target);
-		// program.didRender();
-		// program.draw();
-		// return;
-
-
-
-		//gamma step
-		program = this.gammaProgram;
-		sourceTexture = this.framebuffer1.texture;
-		target = this.framebuffer2;
-
-		program.use();
-		program.update({key: 'gamma', value: 0.02});
-		program.willRender();
-		sourceTexture.use();
-
-		target.use();//this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, target);
-
-		program.didRender();
-		program.draw();
-		// return;
-		
-		if(useDebugger) debugger;
-
-
-		// blend end of saturation step with gamma step to 50%
-		program = this.blendProgram;
-		sourceTexture = this.framebuffer2.texture;
-
-		program.use();
-		program.update({
-			key: 'blend', 
-			amount: 0.5, 
-			blendTexture: this.editGroupFramebuffer.texture,
-		});
-		program.willRender();
-		sourceTexture.use();
-
-		this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
-
-		program.didRender();
-		program.draw();
-
-		if(useDebugger) debugger;
-
-	}
-
-	handleRotate(rotationAdd) {
-		const { width, height } = this.state;
-		const rotate = (this.state.rotate + 360 + rotationAdd) % 360;
-		const canvasWidth = (rotate == 90 || rotate == 270) ? height : width;
-		const canvasHeight = (rotate == 90 || rotate == 270) ? width : height;
-		this.setState({rotate, canvasWidth, canvasHeight}, () => {
-			this.resizeAll();
-			this.renderEditSteps();
-		});
+		this.setState({width, height, canvasWidth, canvasHeight});
 	}
 
 	render() {
-		const { canvasWidth, canvasHeight } = this.state;
+		const { url, instructions } = this.props;
+		const { width, height, canvasWidth, canvasHeight } = this.state;
 		return (
-			<div>
-				<button onClick={() => this.handleRotate(90)}>Rotate Right</button>
-				<button onClick={() => this.handleRotate(-90)}>Rotate Left</button>
-				<br />
-				<canvas ref="editor" width={canvasWidth} height={canvasHeight} />
+			<div style={{zoom: 0.5}}>
+				<Renderer url={url} instructions={instructions} width={width} height={height} canvasWidth={canvasWidth} canvasHeight={canvasHeight} onResize={::this.handleResize} autoResize debug  />
 			</div>
 		)
 	}
+
 }
 
-// let inited = false;
-// document.addEventListener('click', () => {
-// 	if(!inited) {
-		ReactDOM.render(<App /> , document.getElementById('app'));	
-// 		inited = true;
-// 	}
-// });
+ReactDOM.render(<App /> , document.getElementById('app'));
